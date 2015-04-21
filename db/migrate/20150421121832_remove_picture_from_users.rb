2fd31cd6f0f3a2dbf6d7bb8fc26c96b0cca2e644
Hugo Barberot <hugo.barberot@gmail.com>
2015-04-21 18:35:20 +0200

    remove useless migrations

--- db/migrate/20150421121832_remove_picture_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemovePictureFromUsers < ActiveRecord::Migration
-  def change
-    remove_column :users, :picture, :string
-  end
-end
--- db/migrate/20150421122647_remove_omniauth_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemoveOmniauthFromUsers < ActiveRecord::Migration
-  def change
-    remove_column :users, :provider,     :string
-    remove_column :users, :uid,          :string
-    remove_column :users, :name,         :string
-    remove_column :users, :token,        :string
-    remove_column :users, :token_expiry, :datetime
-  end
-end
