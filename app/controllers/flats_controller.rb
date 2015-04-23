b72dcdb115b81afc6d31d522621f1f45d8c090a3
Samir El Maoujoudi <samir.elmaoujoudi@gmail.com>
2015-04-23 15:36:02 +0200

    Add geoloc

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -5,6 +5,7 @@ class FlatsController < ApplicationController
     @markers = Gmaps4rails.build_markers(@flats) do |flat, marker|
       marker.lat flat.latitude
       marker.lng flat.longitude
+      marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
     end
   end
 
