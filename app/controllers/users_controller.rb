35246ca0f8963e9fb0d4b46c5228d6695d0af341
Hugo Barberot <hugo.barberot@gmail.com>
2015-04-23 18:40:39 +0200

    Add account view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,7 @@
 class UsersController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-
-  def destroy
-  end
-
-  def edit
-  end
-
-  def update
-  end
 
   def show
+    @user = User.find(params[:id])
   end
+
 end
