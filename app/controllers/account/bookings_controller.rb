2f3d09957d1efb1be9585ff905c1ae425bb2865e
Hugo Barberot <hugo.barberot@gmail.com>
2015-04-24 15:56:10 +0200

    add total price to bookings

--- app/controllers/account/bookings_controller.rb
+++ app/controllers/account/bookings_controller.rb
@@ -7,6 +7,8 @@ module Account
 
     def create
       @booking = current_user.bookings.build(check_out: params[:check_out].gsub(/\//,'-'), check_in: params[:check_in].gsub(/\//,'-'), flat_id: params[:flat_id])
+      @flat = Flat.find(params[:flat_id])
+      @booking.price_cents = @flat.price_cents * (@booking.check_out - @booking.check_in)
       @booking.save
 
       if @booking.save
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,10 +1,11 @@
 class Booking < ActiveRecord::Base
   belongs_to :user
   belongs_to :flat
+  monetize   :price_cents
 
   validates :check_in,    presence: true
   validates :check_out,   presence: true
-  # validates :total_price, presence: true
-  # validates :user,        presence: true
-  # validates :flat,        presence: true
+  validates :price_cents, presence: true
+  validates :user,        presence: true
+  validates :flat,        presence: true
 end
